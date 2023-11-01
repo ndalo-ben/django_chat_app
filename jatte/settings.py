884b89043c58e5ef835b373359584d127373d807
ben <ndaloben8@gmail.com>
2023-11-01 22:49:36 +0300

    project models

--- jatte/settings.py
+++ jatte/settings.py
@@ -33,6 +33,12 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/chat-admin/'
 LOGOUT_REDIRECT_URL = '/'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    }
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -43,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
     'account',
     'core',
 ]
