232bb84849f341971a018347abfd89bc31cb6522
ben <ndaloben8@gmail.com>
2023-11-04 10:04:48 +0300

    welcome message

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,20 @@
+// Elements 
 
+const chatElement = document.querySelector('#chat')
+const chatOpenElement = document.querySelector('#chat_open')
+const chatJoinElement = document.querySelector('#chat_join')
+const chatIconElement = document.querySelector('#chat_icon')
+const chatWelcomeElement = document.querySelector('#chat_welcome')
+
+
+
+// event listeners 
+
+chatOpenElement.onclick = function(e) {
+    e.preventDefault()
+
+    chatIconElement.classList.add('hidden')
+    chatWelcomeElement.classList.remove('hidden')
+
+    return false
+}
